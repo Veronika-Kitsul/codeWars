977b6518e1b394f995a50b4ea76ceea5227adf63
Veronika Kitsul <65815639+Veronika-Kitsul@users.noreply.github.com>
2021-02-25 14:55:37 -0500

    Dijkstra's

--- CodeWars problems/src/BinarySorting.java
+++ CodeWars problems/src/BinarySorting.java
@@ -3,38 +3,21 @@ public class BinarySorting {
 
 	public BinarySorting() 
 	{
-		removeParentheses("example(unwanted thing)example");
+		removeParentheses("example(unwanted thing)example (blah(boom)oops) world (i)(want) (i ( like (to (be)))) u");
 	}
 	
 	public static String removeParentheses(final String str) 
 	{
-		String cutdown = "\\";
-		int numberOpen = 0;
-		for (int i = 0; i < str.length(); i++)
-		{	
-			if (str.charAt(i) == '(')
-			{
-				numberOpen++;
-			}
-			
-			if (numberOpen > 0)
-			{
-				cutdown+= str.charAt(i);
-			}
-			
-			if (str.charAt(i) == ')')
-			{
-				str.replaceAll(cutdown, "\\");
-				numberOpen--;
-			}
-		}
-		System.out.println(str);
-        return str; 
+		String k = str;
+        while(k.indexOf('(') != -1)
+	    {
+        	k = k.replaceAll("\\([^\\(\\)]*\\)", "");
+	    }
+        return k; 
     }
 
 	 public static void main(String[] args) 
-		{
-			new BinarySorting();
-		}
-
+	{
+		new BinarySorting();
+	}
 }
---
+++ CodeWars problems/src/url_shortener.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class url_shortener 
+{
+	int k = 0;
+	
+	public url_shortener() 
+	{
+		HashMap url = new HashMap();
+		if (k == 0 || k == 1 || k % 27 == 0)
+		{
+			
+			loop();
+		}
+		
+	}
+	
+	public void loop()
+		for (int i = 97; i <= 122; i++)
+		{
+			
+			k++;
+		}
+}
